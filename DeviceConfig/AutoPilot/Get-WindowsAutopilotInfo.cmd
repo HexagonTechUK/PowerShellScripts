939f3ffdec7ac838b1b3bf9ac5500ec2f03d928e
Paul Gosling <psgosling@outlook.com>
2025-05-15 09:14:14 +0100

    Modified: device configs

---
+++ DeviceConfig/AutoPilot/Get-WindowsAutopilotInfo.cmd
@@ -0,0 +1,8 @@
+@echo off
+REM This batch file will execute PowerShell commands as specified
+REM Open PowerShell with ExecutionPolicy Bypass and run the commands
+PowerShell.exe -ExecutionPolicy Bypass -Command "Install-Script -name Get-WindowsAutopilotInfo -Force"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-WindowsAutopilotInfo -Online"
+
+pause
